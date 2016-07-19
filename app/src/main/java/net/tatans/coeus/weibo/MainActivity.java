7c2ec9120cd91d8b05dd093e5ef6db98c4441ade
linchamgman <475463870@qq.com>
2016-07-19 15:26:29 +0800

    上传主布局已经有关的资源文件

--- app/src/main/java/net/tatans/coeus/weibo/MainActivity.java
+++ app/src/main/java/net/tatans/coeus/weibo/MainActivity.java
@@ -1,42 +1,158 @@
 package net.tatans.coeus.weibo;
 
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
 import net.tatans.coeus.network.tools.BaseActivity;
-import net.tatans.rhea.weibo.cache.CacheMainActivity;
-import net.tatans.rhea.network.event.OnClick;
-import net.tatans.rhea.network.view.ContentView;
 
-@ContentView(net.tatans.coeus.weibo.R.layout.main)
-public class MainActivity extends BaseActivity {
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
 
-    @OnClick(net.tatans.coeus.weibo.R.id.bt_global)
-    public void btnGlobal(){
-        /*Intent global_intent = new Intent();
-        global_intent.setClass(this,GlobalActivity.class);
-        startActivity(global_intent);*/
-        TatansStartActivity(GlobalActivity.class);
+    // 获取界面的视图
+    private LinearLayout id_tab_home;
+    private LinearLayout id_tab_message;
+    private LinearLayout id_tab_search;
+    private LinearLayout id_tab_me;
+
+    private ImageButton id_tab_home_img;
+    private ImageButton id_tab_message_img;
+    private ImageButton id_tab_search_img;
+    private ImageButton id_tab_me_img;
+
+    private Fragment mTab01;
+    private Fragment mTab02;
+    private Fragment mTab03;
+    private Fragment mTab04;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        initView();
+        setSelect(0);
     }
 
-    @OnClick(net.tatans.coeus.weibo.R.id.bt_single)
-    public void btnSingle(){
-        /*Intent global_intent = new Intent();
-        global_intent.setClass(this,SingleActivity.class);
-        startActivity(global_intent);*/
-        TatansStartActivity(SingleActivity.class);
+    /**
+     * 初始化控件
+     */
+    private void initView() {
+        id_tab_home = (LinearLayout) findViewById(R.id.id_tab_home);
+        id_tab_message = (LinearLayout) findViewById(R.id.id_tab_message);
+        id_tab_search = (LinearLayout) findViewById(R.id.id_tab_search);
+        id_tab_me = (LinearLayout) findViewById(R.id.id_tab_me);
+
+        id_tab_home_img = (ImageButton) findViewById(R.id.id_tab_home_img);
+        id_tab_message_img = (ImageButton) findViewById(R.id.id_tab_message_img);
+        id_tab_search_img = (ImageButton) findViewById(R.id.id_tab_search_img);
+        id_tab_me_img = (ImageButton) findViewById(R.id.id_tab_me_img);
+
+        // 设置button的监听事件
+        id_tab_home.setOnClickListener(this);
+        id_tab_message.setOnClickListener(this);
+        id_tab_search.setOnClickListener(this);
+        id_tab_me.setOnClickListener(this);
     }
 
-    @OnClick(net.tatans.coeus.weibo.R.id.bt_tiris)
-    public void btTiris(){
-       /* Intent tiris_intent = new Intent();
-        tiris_intent.setClass(this,TestRirisActivity.class);
-        startActivity(tiris_intent);*/
-        TatansStartActivity(TestRirisActivity.class);
-    }
-    @OnClick(net.tatans.coeus.weibo.R.id.bt_cache)
-    public void btCache(){
-        /*Intent tiris_intent = new Intent();
-        tiris_intent.setClass(this,CacheMainActivity.class);
-        startActivity(tiris_intent);*/
-        TatansStartActivity(CacheMainActivity.class);
+    private void setSelect(int i) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        hideFragment(transaction);
+        // 把图片设置为亮的
+        // 设置内容区域
+        switch (i) {
+            case 0:
+                if (mTab01 == null) {
+                    mTab01 = new HomeFragment();
+                    transaction.add(R.id.id_content, mTab01);
+                } else {
+                    transaction.show(mTab01);
+                }
+                break;
+            case 1:
+                if (mTab02 == null) {
+                    mTab02 = new MessageFragment();
+                    transaction.add(R.id.id_content, mTab02);
+                } else {
+                    transaction.show(mTab02);
+                }
+                break;
+            case 2:
+                if (mTab03 == null) {
+                    mTab03 = new FindFragment();
+                    transaction.add(R.id.id_content, mTab03);
+                } else {
+                    transaction.show(mTab03);
+                }
+                break;
+            case 3:
+                if (mTab04 == null) {
+                    mTab04 = new MeFragment();
+                    transaction.add(R.id.id_content, mTab04);
+                } else {
+                    transaction.show(mTab04);
+                }
+                break;
+            default:
+                break;
+        }
+        transaction.commit();
     }
 
+    /**
+     * 隐藏掉不是显示tab的其他内容
+     */
+    private void hideFragment(FragmentTransaction transaction) {
+        if (mTab01 != null) {
+            transaction.hide(mTab01);
+        }
+        if (mTab02 != null) {
+            transaction.hide(mTab02);
+        }
+        if (mTab03 != null) {
+            transaction.hide(mTab03);
+        }
+        if (mTab04 != null) {
+            transaction.hide(mTab04);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+//        resetImgs();
+        switch (v.getId()) {
+            case R.id.id_tab_home:
+                setSelect(0);
+                break;
+            case R.id.id_tab_message:
+                setSelect(1);
+                break;
+            case R.id.id_tab_search:
+                setSelect(2);
+                break;
+            case R.id.id_tab_me:
+                setSelect(3);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 切换图片至暗色
+     */
+//    private void resetImgs() {
+//        id_tab_weixin_img.setImageResource(R.drawable.tab_weixin_normal);
+//        id_tab_frd_img.setImageResource(R.drawable.tab_find_frd_normal);
+//        id_tab_address_img.setImageResource(R.drawable.tab_address_normal);
+//        id_tab_settings_img.setImageResource(R.drawable.tab_settings_normal);
+//    }
 }
