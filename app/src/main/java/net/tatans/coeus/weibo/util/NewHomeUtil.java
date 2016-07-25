03861b0d924939fc45d6c2c950f98fc6cdc43a46
Administrator <15727637389@163.com>
2016-07-25 15:16:38 +0800

    微博首页搭建

---
+++ app/src/main/java/net/tatans/coeus/weibo/util/NewHomeUtil.java
@@ -0,0 +1,85 @@
+package net.tatans.coeus.weibo.util;
+
+public class NewHomeUtil {
+
+	private String me_created_at;
+	private String me_text;
+	private String me_source;
+	private String me_original_pic;
+	private String me_user;
+	private String me_id;
+
+
+
+	private String he_created_at;
+	private String he_text;
+	private String he_source;
+	private String heoriginal_pic;
+	private String he_user;
+	public String getMe_created_at() {
+		return me_created_at;
+	}
+	public void setMe_created_at(String me_created_at) {
+		this.me_created_at = me_created_at;
+	}
+	public String getMe_text() {
+		return me_text;
+	}
+	public void setMe_text(String me_text) {
+		this.me_text = me_text;
+	}
+	public String getMe_source() {
+		return me_source;
+	}
+	public void setMe_source(String me_source) {
+		this.me_source = me_source;
+	}
+	public String getMe_original_pic() {
+		return me_original_pic;
+	}
+	public void setMe_original_pic(String me_original_pic) {
+		this.me_original_pic = me_original_pic;
+	}
+	public String getMe_user() {
+		return me_user;
+	}
+	public void setMe_user(String me_user) {
+		this.me_user = me_user;
+	}
+	public String getHe_created_at() {
+		return he_created_at;
+	}
+	public void setHe_created_at(String he_created_at) {
+		this.he_created_at = he_created_at;
+	}
+	public String getHe_text() {
+		return he_text;
+	}
+	public void setHe_text(String he_text) {
+		this.he_text = he_text;
+	}
+	public String getHe_source() {
+		return he_source;
+	}
+	public void setHe_source(String he_source) {
+		this.he_source = he_source;
+	}
+	public String getHeoriginal_pic() {
+		return heoriginal_pic;
+	}
+	public void setHeoriginal_pic(String heoriginal_pic) {
+		this.heoriginal_pic = heoriginal_pic;
+	}
+	public String getHe_user() {
+		return he_user;
+	}
+	public void setHe_user(String he_user) {
+		this.he_user = he_user;
+	}
+	public String getMe_id() {
+		return me_id;
+	}
+	public void setMe_id(String me_id) {
+		this.me_id = me_id;
+	}
+}
