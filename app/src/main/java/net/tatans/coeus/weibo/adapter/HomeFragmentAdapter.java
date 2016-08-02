d075fb7ddd8430f878fcb830ad204003c9aef907
Administrator <15727637389@163.com>
2016-08-02 12:57:00 +0800

    首页链接点击处理好

--- app/src/main/java/net/tatans/coeus/weibo/adapter/HomeFragmentAdapter.java
+++ app/src/main/java/net/tatans/coeus/weibo/adapter/HomeFragmentAdapter.java
@@ -4,10 +4,10 @@ package net.tatans.coeus.weibo.adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.TextPaint;
-import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.view.LayoutInflater;
@@ -17,33 +17,17 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.sina.weibo.sdk.auth.Oauth2AccessToken;
-import com.sina.weibo.sdk.exception.WeiboException;
-import com.sina.weibo.sdk.net.RequestListener;
-import com.sina.weibo.sdk.openapi.legacy.ShortUrlAPI;
 import com.sina.weibo.sdk.openapi.models.Status;
 import com.sina.weibo.sdk.openapi.models.StatusList;
 import com.squareup.picasso.Picasso;
 
-import net.tatans.coeus.speech.dto.UnderstandResultDto;
 import net.tatans.coeus.weibo.R;
-import net.tatans.coeus.weibo.activity.LinkShowActivity;
-import net.tatans.coeus.weibo.util.Constants;
-import net.tatans.coeus.weibo.util.NewHomeUtil;
 import net.tatans.coeus.weibo.util.TimeFormat;
 
-import java.text.ParseException;
-import java.text.ParsePosition;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -58,10 +42,9 @@ public class HomeFragmentAdapter extends BaseAdapter {
     private String me_urlhttp = null;
     private String he_urlhttp = null;
     private Oauth2AccessToken mAccessToken;
-    private int tag=0;
     //正则表达式匹配规则
     private Pattern pattern= Pattern
-            .compile("(http://|ftp://|https://|www){0,1}([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?");
+            .compile("(http://|ftp://|https://|www){1}([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?");
 
     public HomeFragmentAdapter(Context context, List<StatusList> list,Oauth2AccessToken AccessToken) {
         this.mContext = context;
@@ -109,16 +92,13 @@ public class HomeFragmentAdapter extends BaseAdapter {
         holder.home_page_usercontent.setText(status.text);
         String str=status.text;
         while (mt_me.find()) {
-            System.out.println(mt_me.group(tag));
-            String vvv=mt_me.group(tag);
-            str=str.replace(vvv,"网页链接");
-            SpannableString ss = new SpannableString(str);
-            ss.setSpan(span, str.indexOf("网页链接"), str.indexOf("网页链接") + 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            holder.home_page_usercontent.setText(ss);
+            String mgroup=mt_me.group(0);
+            str=str.replace(mgroup,"网页链接");
+            SpannableString spannableString = new SpannableString(str);
+            spannableString.setSpan(new HomeSpan(mgroup), str.indexOf("网页链接"), str.indexOf("网页链接") + 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            holder.home_page_usercontent.setText(spannableString);
             holder.home_page_usercontent.setMovementMethod(LinkMovementMethod.getInstance());
-            tag+=1;
         }
-        tag=0;
         String time = TimeFormat.dTime(status.created_at);
         holder.home_page_username.setText(status.user.name);
         holder.home_page_usertime.setText(time);
@@ -133,6 +113,7 @@ public class HomeFragmentAdapter extends BaseAdapter {
             Picasso.with(mContext).load(status.original_pic).resize(100, 100).placeholder(R.drawable.icon_image_model_short).resize(100, 100).error(R.drawable.icon_image_model_short).resize(100, 100).into(holder.home_page_pic);
             holder.home_page_pic_text.setText(status.pic_urls.size() + "张图片点击查看");
         }
+        /**这里主要对转发的处理*/
         if (status.retweeted_status == null) {
             holder.home_page_he_user.setVisibility(View.GONE);
             holder.home_page_usercomments.setVisibility(View.GONE);
@@ -154,15 +135,13 @@ public class HomeFragmentAdapter extends BaseAdapter {
                 Matcher mt_he = pattern.matcher(status.retweeted_status.text);
                 String strs=status.retweeted_status.text;
                 while (mt_he.find()) {
-                    String vvv=mt_me.group(tag);
-                    strs=strs.replace(vvv,"网页链接");
-                    SpannableString ss = new SpannableString(str);
-                    ss.setSpan(span, str.indexOf("网页链接"), str.indexOf("网页链接") + 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                    holder.home_page_usercontent.setText(ss);
-                    holder.home_page_usercontent.setMovementMethod(LinkMovementMethod.getInstance());
-                    tag+=1;
+                    String mgroup=mt_he.group(0);
+                    strs=strs.replace(mgroup,"网页链接");
+                    SpannableString spannableString = new SpannableString(strs);
+                    spannableString.setSpan(new HomeSpan(mgroup), strs.indexOf("网页链接"), strs.indexOf("网页链接") + 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    holder.home_page_usercomments.setText(spannableString);
+                    holder.home_page_usercomments.setMovementMethod(LinkMovementMethod.getInstance());
                 }
-                tag=0;
                 if (status.retweeted_status.original_pic == null) {
                     holder.home_page_he_pic.setVisibility(View.GONE);
                     holder.home_page_he_pic_text.setVisibility(View.GONE);
@@ -217,7 +196,12 @@ public class HomeFragmentAdapter extends BaseAdapter {
         private ImageView home_page_he_pic;
         private TextView home_page_he_pic_text;
     }
-   final ClickableSpan span=new ClickableSpan() {
+    class HomeSpan extends ClickableSpan{
+        String mtext;
+        public HomeSpan(String text){
+            this.mtext=text;
+
+        }
         @Override
         public void updateDrawState(TextPaint ds) {
             super.updateDrawState(ds);
@@ -227,6 +211,9 @@ public class HomeFragmentAdapter extends BaseAdapter {
 
         @Override
         public void onClick(View widget) {
+            final Uri uri = Uri.parse(mtext);
+            final Intent it = new Intent(Intent.ACTION_VIEW, uri);
+            mContext.startActivity(it);
         }
-    };
+    }
 }
