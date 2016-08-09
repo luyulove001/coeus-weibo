6d5b5f006d5390d4b31355402d0fbbfc3349f2d1
linchamgman <475463870@qq.com>
2016-08-09 15:07:49 +0800

    得到高清图

--- app/src/main/java/net/tatans/coeus/weibo/activity/ImagesActivity.java
+++ app/src/main/java/net/tatans/coeus/weibo/activity/ImagesActivity.java
@@ -6,10 +6,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.widget.ImageView;
-
-import com.iflytek.cloud.Setting;
 
 import net.tatans.coeus.network.tools.BaseActivity;
 import net.tatans.coeus.weibo.R;
